e08c4390211ac4986f7509d52d7de13c835c4833
Einar Lielmanis <einars@gmail.com>
2011-02-05 03:45:18 +0200

    Initial template

---
+++ Makefile
@@ -0,0 +1,8 @@
+SOURCES = skapong.ml
+RESULT  = skapong
+
+LIBS = graphics
+
+include OCamlMakefile
+
+
---
+++ skapong.ml
@@ -0,0 +1,12 @@
+open Printf
+open Graphics
+
+
+let main () =
+    ()
+
+
+let _ = main ()
+
+(* vim: set tw=0 : *)
+
